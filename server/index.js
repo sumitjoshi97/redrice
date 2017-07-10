957e1cb0640b18e3a0668483986728725c1aa9f3
Sumit Joshi <sumitjoshi479@gmail.com>
2017-07-10 15:21:04 +0530

    rendered first react component

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import path from 'path';
+import webpack from 'webpack';
+import webpackMiddleWare from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
 
 let app = express();
 
+app.use(webpackMiddleWare(webpack(webpackConfig)));
+
 app.get('/', (req,res) => {
-    res.send(path.join(__dirname, './index.html'))
-})
+    res.sendFile(path.join(__dirname, './index.html'));
+});
 
 app.listen(8000, () => console.log('Running on 8000'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+import path from 'path';
+
+export default {
+    entry: path.join(__dirname, '/client/index.js'),
+    output: {
+        path: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsX?$/,
+                include: path.join(__dirname, 'client'),
+                loaders: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.','.js']
+    }
+}
